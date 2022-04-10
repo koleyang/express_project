1bc5388571294de28a0f66e9eda189cb69baf1f6
koleyang <koleyang@126.com>
2022-04-10 14:26:58 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  // res.send('welcome to Express.')
+  res.status(200).json({
+    code: 1,
+    message: '路由访问成功.'
+  });
 });
 
 module.exports = router;
